2dae0fd1d8d754448e83216972244016a7b88d25
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-11-01 22:27:41 +0330

    feat: non-vectorized gram schmidt process added

--- 02_gram_schmidt_process/gram_schmidt.py
+++ 02_gram_schmidt_process/gram_schmidt.py
@@ -1,31 +1,40 @@
 import numpy as np
 
+def coeff(vi, v):
+	return np.dot(vi, v) / np.dot(v, v)
+
 def orthogonolize(U):
 	"""
 	Orthogonolize the matrix U (d x n) using Gram-Schmidt process.
 
-	Args:
-		U (numpy.array): d x n matrix with columns that need to be orthogonolized.
-
-	Returns:
-		(numpy.array): d x n orthogonal matrix.
 	"""
 
-	V = np.array([])
 	## TODO ##
-	#################
-
-	#################
+	##################################
+	V = []
+    for x in X:
+        tmp = x
+        for v in V:
+            proj = coff(x, v) * v
+            tmp = tmp - proj
+        V.append(tmp)
+	##################################
 
 	return V
 
 
 if __name__ == "__main__":
 
-	U = np.array(([[3, 1],
-				  [2, 1]]))
+	# each row is a vector
+	U1 = np.array([[3.0, 1.0],
+                   [2.0, 2.0]])
 
-	V = orthogonolize(U)
-	print(V)
+	U2 = np.array([[1.0, 1.0, 0.0],
+                   [1.0, 3.0, 1.0],
+                   [2.0, -1.0, 1.0]])
 
-	assert V.shape == U.shape
+	V1 = orthogonolize(U1)
+	print(V1)
+
+	V2 = orthogonolize(U2)
+	print(V2)
